0b224b2296de052627558f324912906cde0e49b1
ogama8 <ogama8@gmail.com>
2016-05-20 13:35:11 -0700

    Added protections in case of accidental premature exit

--- fade_bg.sh
+++ fade_bg.sh
@@ -1,28 +1,32 @@
-#!/bin/bash
+#!/bin/bash ####################################################################
 
 cd ~/pictures/backgrounds/temp
+
+if [ -f fade1000.jpg ]
+then
+   for i in $(seq -f "%04g" 0 1000)
+   do
+      feh --bg-fill fade$i.jpg
+   done
+fi
+
+rm fade*.jpg
 
 NEW=$(find ../ -maxdepth 1 -type f ! -name '*_o*')
 NEW=$(shuf -n1 -e $NEW)
 cp $NEW ./_new.jpg
 
-for i in $(seq -f "%03g" 0 500)
+for i in $(seq -f "%04g" 0 1000)
 do
-   composite -displace $i _new.jpg _cur.jpg fade$i.jpg
-   if [ $i -gt 250 ]
+   composite -displace $(echo "$i / 2" | bc -l) _new.jpg _cur.jpg fade$i.jpg
+   if [ $i -gt 500 ]
    then
-      ADJ=$(echo "$i - 250" | bc)
-      composite -dissolve $(echo "$ADJ / 2.5" | bc) _new.jpg fade$i.jpg fade$i.jpg
+      ADJ=$(echo "$i - 500" | bc -l)
+      composite -dissolve $(echo "$ADJ / 5" | bc -l) \
+               _new.jpg fade$i.jpg fade$i.jpg
    fi
 done
 
 mv _new.jpg _cur.jpg
 
-
-for i in $(seq -f "%03g" 0 500)
-do
-   feh --bg-fill fade$i.jpg
-done
-
-rm fade*.jpg
 
