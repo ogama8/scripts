bad6b0f55429d3b89263a371a4fc36a516614b08
ogama8 <ogama8@gmail.com>
2016-05-20 18:21:15 -0700

    Working through cron now

--- fade_bg.sh
+++ fade_bg.sh
@@ -1,6 +1,10 @@
 #!/bin/bash ####################################################################
 
+export DISPLAY=:0  # Change as needed; ie. "echo $DISPLAY" from a fresh term ###
+
 cd ~/pictures/backgrounds/temp
+
+INDEX=$(ls | grep '[0-9]' | sort -r | head -n 1 | tr -d -c [0-9])
 
 if [ -f fade1000.jpg ]
 then
@@ -8,25 +12,31 @@ then
    do
       feh --bg-fill fade$i.jpg
    done
+
+   rm fade*.jpg
+   mv _new.jpg _cur.jpg
+
+   if [ $RANDOM -gt $RANDOM ]
+   then
+      wget -O _new.jpg https://source.unsplash.com/random/1920x1080
+   else
+      NEW=$(find ../ -maxdepth 1 -type f ! -name '*_o*')
+      NEW=$(shuf -n1 -e $NEW)
+      cp $NEW ./_new.jpg
+   fi
+
+   INDEX=0
 fi
 
-rm fade*.jpg
 
-NEW=$(find ../ -maxdepth 1 -type f ! -name '*_o*')
-NEW=$(shuf -n1 -e $NEW)
-cp $NEW ./_new.jpg
-
-for i in $(seq -f "%04g" 0 1000)
+for i in $(seq -f "%04g" $INDEX 1000)
 do
    composite -displace $(echo "$i / 2" | bc -l) _new.jpg _cur.jpg fade$i.jpg
    if [ $i -gt 500 ]
    then
       ADJ=$(echo "$i - 500" | bc -l)
       composite -dissolve $(echo "$ADJ / 5" | bc -l) \
-               _new.jpg fade$i.jpg fade$i.jpg
+               _new.jpg fade$i.jpg fade$i.jpg &
    fi
 done
-
-mv _new.jpg _cur.jpg
-
 
