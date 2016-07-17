566bb7162ebeccd8ea151afc6543f3add9f80deb
ogama8 <ogama8@gmail.com>
2016-07-17 15:41:54 -0700

    updated style

--- cur_fade.sh
+++ cur_fade.sh
@@ -6,11 +6,11 @@ if [ ! -f fade1000.jpg ]
 then
    echo -n '<span foreground="#222222" background="#bbbbbb">'
    echo -n $(ls | grep '[2-9]' | sort -r | head -n 1 | tr -d -c [0-9])
-   echo '</span>▌'
+   echo '▐</span>'
 elif [ ! -f _right/fade1000.jpg ]
 then
    echo -n '<span foreground="#a0c060"></span><span foreground="#222222" background="#a0c060">'
    echo -n $(ls _right/ | grep '[1-9]' | sort -r | head -n 1 | tr -d -c [0-9])
-   echo '</span><span foreground="#a0c060">▌</span>'
+   echo '▐</span>'
 fi
 
