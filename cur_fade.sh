36bdd9d7bf3489629041bcd1e992ecb5a6d21da6
ogama8 <ogama8@gmail.com>
2016-08-04 10:06:00 -0700

    trigger_fade won't run unless all fades have been generated

--- cur_fade.sh
+++ cur_fade.sh
@@ -7,10 +7,14 @@ then
    echo -n '<span foreground="#222222" background="#bbbbbb">'
    echo -n $(ls | grep '[2-9]' | sort -r | head -n 1 | tr -d -c [0-9])
    echo '▐</span>'
+   exit 0
 elif [ ! -f _right/fade1000.jpg ]
 then
    echo -n '<span foreground="#a0c060"></span><span foreground="#222222" background="#a0c060">'
    echo -n $(ls _right/ | grep '[1-9]' | sort -r | head -n 1 | tr -d -c [0-9])
    echo '▐</span>'
+   exit 0
+else
+   exit 1
 fi
 
--- trigger_fade.sh
+++ trigger_fade.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if sh ~/scripts/cur_fade.sh > /dev/null
+then
+   (echo 'Fade not ready.') | dzen2 -bg '#222222' -fg '#bbbbbb' -x 850 -y 905 -w 220 -h 26 -p 3 -fn '-*-terminess powerline-medium-r-normal--*-*-*-*-*-*-*-1' &
+   rm /tmp/trigger_fade
+   exit 1
+fi
+
 sh ~/scripts/notify_trigger_fade.sh &
 
 for i in $(seq 0 60)
