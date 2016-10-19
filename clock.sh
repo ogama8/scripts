e293c63d5d37605cd6de74d4ff221029b020e446
ogama8 <ogama8@gmail.com>
2016-10-19 14:29:44 -0400

    now counts down to midnight

--- clock.sh
+++ clock.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-(while true; do date +%T.%3N; sleep 0.0005715; done) | dzen2 -bg darkred -fg grey80 -fn fixed -x 60 -y 390 -w 1800 -h 300 -fn '-*-DejaVu Sans-*-r-*-*-220-*-*-*-*-*-*-*'
+function panic(){
+   while true
+   do
+      printf $(date -u --date @$((86400 - (`date +%s` + 3600*$1)%86400)) +%H:%M:%S | tr -d '\n')
+      printf ".%03d\n" $((999 - 10#`date +%03N`))        # the 10# forces the result to be in
+                                                         # base 10, which would otherwise be
+                                                         # interpreted as octal
+   done
+}
+
+panic -4 | dzen2 -bg darkred -fg grey80 -fn fixed -x 60 -y 390 -w 1800 -h 300 -fn '-*-DejaVu Sans-*-r-*-*-220-*-*-*-*-*-*-*'
 
