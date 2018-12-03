9af5914f27e52022bb364f6464db7a8947fe02d2
ogama8 <ogama8@gmail.com>
2018-12-03 01:25:41 -0500

    pdf downsize added

---
+++ pdf_downsize.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${1%.*}_small.pdf $1
---
+++ xrandr.sh
@@ -0,0 +1,3 @@
+xrandr --output DVI-I-1 --auto --pos 0000x1080 \
+       --output DP-1    --auto --pos 2000x0000 --rotate right \
+       --output HDMI-0  --size 1920x1080 --rate 60 --pos 0000x0000
